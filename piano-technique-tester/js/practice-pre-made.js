894faa505f8f4313ff7f706c9c8f27ad9bdce515
amberking3 <30532560+amberking3@users.noreply.github.com>
2018-03-10 18:09:32 -0500

    edit

--- piano-technique-tester/js/practice-pre-made.js
+++ piano-technique-tester/js/practice-pre-made.js
@@ -54,9 +54,6 @@ $(document).ready(function() {
                 listItems = level10.slice();
         }
         
-        var popover = $(".p-list-display-btn").data("amber");
-        popover.setContent();
-        
         function shuffleArray(array) {
             for (var i = array.length - 1; i > 0; i--) {
                 var j = Math.floor(Math.random() * (i + 1));
